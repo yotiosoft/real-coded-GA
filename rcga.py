3cf5077ccc2f3e051c58bb7eef93c8bf5e942f8c
YotioSoft <ytani0323@gmail.com>
2023-07-22 17:09:36 +0900

    make

--- rcga.py
+++ rcga.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 # BLX-α 交叉
 def blx_alpha(x1, x2, pc, alpha=0.5):
@@ -27,7 +28,11 @@ def blx_alpha(x1, x2, pc, alpha=0.5):
 
 # 評価関数
 def rosenbrock(x):
-    return sum(100 * (x[1:] - x[:-1] ** 2) ** 2 + (1 - x[:-1]) ** 2)
+    sum = 0
+    for i in range(DIM-1):
+        sum += 100 * (x[i + 1] - x[i] ** 2) ** 2 + (1 - x[i]) ** 2
+    return sum
+    #return sum(100 * (x[1:] - x[:-1] ** 2) ** 2 + (1 - x[:-1]) ** 2)
 
 # dim = 50
 DIM = 50
@@ -45,22 +50,32 @@ for i in range(CELL):
 # 遺伝的アルゴリズムの実行
 for g in range(1000):
     x_values = np.zeros(CELL, dtype=np.float64)
+    x_min = math.inf
     for i in range(CELL):
         # 評価関数の値を計算
         x_values[i] = rosenbrock(x[i])
+        if x_min > x_values[i]:
+            x_min = x_values[i]
+    print("Generation: {0}, Minimum: {1}".format(g, x_min))
+
     # 次の世代の個体群を生成する
     # ここではトーナメント選択を採用
     # 次の世代の個体群の個数 = CELL
     # 取り出す数 = 2
     new_x = np.zeros((CELL, DIM), dtype=np.float64)
     for i in range(CELL):
-        x_choised = np.random.choise(CELL, 2, replace=False)
-        if x_values[x_choised[0]] < x_values[x_choised[1]]:
-            x1 = x[x_choised[0]]
+        x_choiced = np.random.choice(CELL, 2, replace=False)
+        if x_values[x_choiced[0]] < x_values[x_choiced[1]]:
+            x1 = x[x_choiced[0]]
         else:
-            x1 = x[x_choised[1]]
+            x1 = x[x_choiced[1]]
         new_x[i] = x1
+
     # 交叉
     # ランダムに2つの個体を選択し、交叉率Pcの確率で交叉を行う
     crossover_x = np.random.choice(CELL, 2, replace=False)
     new_x[crossover_x[0]], new_x[crossover_x[1]] = blx_alpha(new_x[crossover_x[0]], new_x[crossover_x[1]], Pc)
+
+    x = new_x
+
+# 最終的な個体群の中で最も評価関数の値が小さい個体を選択
