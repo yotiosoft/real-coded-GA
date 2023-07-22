fca447db8e51d8e2bf2355ce5b3756fd647719e4
YotioSoft <ytani0323@gmail.com>
2023-07-22 18:18:52 +0900

    apply the Just-Generation-Gap model

--- rcga.py
+++ rcga.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 # BLX-α 交叉
-def blx_alpha(x1, x2, pc, alpha=0.5):
+def blx_alpha(x1, x2, pc, alpha=0.01):
     c1 = np.zeros(DIM, dtype=np.float64)
     c2 = np.zeros(DIM, dtype=np.float64)
     # 交叉率pcの確率で交叉を行う
@@ -58,26 +58,31 @@ for g in range(1000):
             x_min = x_values[i]
     print("Generation: {0}, Minimum: {1}".format(g, x_min))
 
-    # 次の世代の個体群を生成する
-    # ここではトーナメント選択を採用
-    # 次の世代の個体群の個数 = CELL
-    # 取り出す数 = 2
-    new_x = np.zeros((CELL, DIM), dtype=np.float64)
-    for i in range(CELL):
-        x_choiced = np.random.choice(CELL, 2, replace=False)
-        if x_values[x_choiced[0]] < x_values[x_choiced[1]]:
-            x1 = x[x_choiced[0]]
-        else:
-            x1 = x[x_choiced[1]]
-        new_x[i] = x1
+    # 親世代をランダムに抽出
+    # 抽出数 = n_p
+    n_p = 50
+    x_parent_index = np.random.randint(0, CELL, n_p)
+    x_parent = x[x_parent_index]
 
     # 交叉
-    # ランダムに2つの個体を選択し、交叉率Pcの確率で交叉を行う
-    crossover_x = np.random.choice(CELL, 2, replace=False)
-    print(new_x[crossover_x[0]], new_x[crossover_x[1]])
-    new_x[crossover_x[0]], new_x[crossover_x[1]] = blx_alpha(new_x[crossover_x[0]], new_x[crossover_x[1]], Pc)
-    print(new_x[crossover_x[0]], new_x[crossover_x[1]])
-    
-    x = new_x
+    # 個体数は DIM * 10
+    child = np.zeros((DIM * 10, DIM), dtype=np.float64)
+    child_values = np.zeros(DIM * 10, dtype=np.float64)
+    for i in range(0, DIM * 10, 2):
+        # ランダムに2つの個体を選択し、交叉率Pcの確率で交叉を行う
+        crossover_x = np.random.randint(0, n_p, 2)
+        child[i], child[i+1] = blx_alpha(x_parent[crossover_x[0]], x_parent[crossover_x[1]], Pc)
+        child_values[i] = rosenbrock(child[i])
+        child_values[i+1] = rosenbrock(child[i+1])
+
+    # エリートを選択
+    # エリート数 = n_p
+    # まずは child を評価値の小さい順にソート
+    child = child[np.argsort(child_values)]
+    # 次に 0 ~ np-1 番目の個体をエリートとする
+    elite = child[:n_p]
+
+    # x_parent を elite で置き換える
+    x[x_parent_index] = elite
 
 # 最終的な個体群の中で最も評価関数の値が小さい個体を選択
