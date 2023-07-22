5f1b99e8b341e17c48808f6ce79cddabc59ae6ef
YotioSoft <ytani0323@gmail.com>
2023-07-22 13:44:56 +0900

    rand

--- rcga.py
+++ rcga.py
@@ -6,3 +6,8 @@ DIM = 50
 CELL = 300
 
 x = np.zeros((CELL, DIM), dtype=np.float64)
+# x を [0, 1] の間でランダムに初期化
+for i in range(CELL):
+    for j in range(DIM):
+        x[i][j] = np.random.rand()
+print(x)    
